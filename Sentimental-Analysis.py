5dc5f5f454ca163a4643aba452ffc7118ed5c1e0
Avi Dipransh <avidipransh@gmail.com>
2019-09-02 10:54:40 +0530

    Removed the api keys

--- Sentimental-Analysis.py
+++ Sentimental-Analysis.py
@@ -65,10 +65,10 @@ class SentimentalAnalyser:
 
   def getTweets(self , topic):
     tweets = []
-    consumer_key = "4HZz7KabLQiMfsfn8VsBjdp6z"
-    consumer_secret = "VWj3sFWcKkVq8E2Gdr8fNsoGMeZCjhVwCXE8BTEWpy0LLOcxWI"
-    access_token = "716622864741449728-bI6HI3YwTuCpZPZSRPqA7m00w7BDNTV"
-    access_token_secret = "7QeDWNjMTnhBJt4mlS8HTMLHuwHOkSu5LJx8WKI7uhCvy"
+    consumer_key = "##########3"
+    consumer_secret = "##########"
+    access_token = "#################-########"
+    access_token_secret = "##############################"
     try:
         auth = OAuthHandler(consumer_key,consumer_secret)
         auth.set_access_token(access_token , access_token_secret)
@@ -97,9 +97,11 @@ def main():
   tweets = ob.getTweets(topic)
 
   test_tweets = ob.cleanTestTweets(tweets)
-  test_tweets = np.transpose(test_tweets)
-  test_tweets.reshape(-1,1)
-  results = model.predict(test_tweets)
+  cv = CountVectorizer(max_features = 3000)
+  X = cv.fit_transform(test_tweets)
+  import pdb
+  pdb.set_trace()
+  results = model.predict(X.toarray())
 
   print(results)
 
